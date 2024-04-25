25d5667b7a78a65cfbb069db46025993103d8dfa
Lucca Freitas <luccafreitas@Luccas-MacBook-Air.local>
2024-04-24 22:45:21 -0300

    projeto implementado

---
+++ my-app/src/app/animal-item/animal-item.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { Animal } from '../models/animal';
+
+@Component({
+  selector: 'app-animal-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './animal-item.component.html',
+  styleUrl: './animal-item.component.css'
+})
+export class AnimalItemComponent {
+
+  constructor() {
+    this.animal = new Animal('Mingau', 'Felino', 18, 'Macho')
+  }
+
+  @Input() animal: Animal = new Animal('', '', 0, '');
+}
+
